84c91985717dee20b900c487cbf8f432a8f1c527
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2023-03-05 23:17:59 -0800

    Big fixes

--- utils/prometheus-exporter/files/periodics/iperf3.lua
+++ utils/prometheus-exporter/files/periodics/iperf3.lua
@@ -1,38 +1,40 @@
 #!/usr/bin/lua
 --[[
 
-	Part of AREDN -- Used for creating Amateur Radio Emergency Data Networks
-	Copyright (C) 2023 Tim Wilkinson
-	See Contributors file for additional contributors
+    Part of AREDN -- Used for creating Amateur Radio Emergency Data Networks
+    Copyright (C) 2023 Tim Wilkinson
+    See Contributors file for additional contributors
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation version 3 of the License.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation version 3 of the License.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-	Additional Terms:
+    Additional Terms:
 
-	Additional use restrictions exist on the AREDN(TM) trademark and logo.
-		See AREDNLicense.txt for more info.
+    Additional use restrictions exist on the AREDN(TM) trademark and logo.
+        See AREDNLicense.txt for more info.
 
-	Attributions to the AREDN Project must be retained in the source code.
-	If importing this code into a new or existing project attribution
-	to the AREDN project must be added to the source code.
+    Attributions to the AREDN Project must be retained in the source code.
+    If importing this code into a new or existing project attribution
+    to the AREDN project must be added to the source code.
 
-	You must not misrepresent the origin of the material contained within.
+    You must not misrepresent the origin of the material contained within.
 
-	Modified versions must be modified to attribute to the original source
-	and be marked in reasonable ways as differentiate it from the original
-	version
+    Modified versions must be modified to attribute to the original source
+    and be marked in reasonable ways as differentiate it from the original
+    version
 
 --]]
+
+local info = require("aredn.info")
 
 local M = {}
 local peers = nil
@@ -55,56 +57,57 @@ function M.is_peer(hostname)
             end
         end
     end
-    return peers[hostname]
+    return peers[hostname:lower()]
 end
 
 function M.run_iperf3_metric(from, to, protocol)
-	local bitrate = nil
-	local retry = 3
-	while not bitrate and retry > 0
-	do
-		local f = io.popen("wget -q -O - 'http://" .. from .. ":8080/cgi-bin/iperf?server=" .. to .. "&protocol=" .. protocol .. "'")
-		if f then
-			for line in f:lines()
-			do
-				if line:match("iperf is disabled") then
-					break
-				end
-				if line:match("<title>BUSY</title>") then
-					f:close()
-					f = nil
-					retry = retry - 1
-					wait_for_ticks(math.floor(20 + 20 * math.random()))
-					break
-				end
-				local bitrate, bu = line:match("[%d%.]+-[%d%.]+%s+sec%s+[%d%.]+ [KM]Bytes%s+([%d%.]+) ([MK])bits/sec%s+receiver")
-				if bitrate then
-					bitrate = tonumber(bitrate) / (bu == "K" and 1024 or 1)
-					break
-				end
-				local bitrate, bu = line:match("[%d%.]+-[%d%.]+%s+sec%s+[%d%.]+ [KM]Bytes%s+([%d%.]+) ([MK])bits/sec%s+[%d%.]+ ms%s+%d+/%d+ %(([%d%.]+)%%%)%s+receiver")
-				if bitrate then
-					bitrate = tonumber(bitrate) / (bu == "K" and 1024 or 1)
-					break
-				end
-			end
-			if f then
-				f:close()
-			end
-		end
-	end
-	return bitrate
+    local bitrate = nil
+    local bu
+    local retry = 3
+    while not bitrate and retry > 0
+    do
+        local f = io.popen("wget -q -O - 'http://" .. from .. ":8080/cgi-bin/iperf?server=" .. to .. "&protocol=" .. protocol .. "'")
+        if f then
+            for line in f:lines()
+            do
+                if line:match("iperf is disabled") then
+                    break
+                end
+                if line:match("<title>BUSY</title>") then
+                    f:close()
+                    f = nil
+                    retry = retry - 1
+                    wait_for_ticks(math.floor(20 + 20 * math.random()))
+                    break
+                end
+                bitrate, bu = line:match("[%d%.]+-[%d%.]+%s+sec%s+[%d%.]+ [KM]Bytes%s+([%d%.]+) ([MK])bits/sec%s+receiver")
+                if bitrate then
+                    bitrate = tonumber(bitrate) / (bu == "K" and 1024 or 1)
+                    break
+                end
+                bitrate, bu = line:match("[%d%.]+-[%d%.]+%s+sec%s+[%d%.]+ [KM]Bytes%s+([%d%.]+) ([MK])bits/sec%s+[%d%.]+ ms%s+%d+/%d+ %(([%d%.]+)%%%)%s+receiver")
+                if bitrate then
+                    bitrate = tonumber(bitrate) / (bu == "K" and 1024 or 1)
+                    break
+                end
+            end
+            if f then
+                f:close()
+            end
+        end
+    end
+    return bitrate
 end
 
 function M.task(conf, metrics, opt)
 
-	peers = nil
+    peers = nil
 
-	for _, c in ipairs(conf)
-	do
-		if c.name == "iperf3" and (c.frequency == "hourly" or (c.frequency == "daily" and opt.is_daily)) then
-            local hostname, protocol = c.arg:match("(%S+)%s+(%S+)")
-            if hostname and is_peer(hostname) then
+    for _, c in ipairs(conf)
+    do
+        if c.name == "iperf3" and (c.frequency == "hourly" or (c.frequency == "daily" and opt.is_daily)) then
+            local hostname, protocol = c.arg:match("^(%S+)%s*(%S*)%s*")
+            if hostname and M.is_peer(hostname) then
                 if protocol ~= "udp" then
                     protocol = "tcp"
                 end
@@ -114,11 +117,11 @@ function M.task(conf, metrics, opt)
                     wait_for_ticks(1)
                     tx = M.run_iperf3_metric(hostname, myhostname, protocol)
                 end
-                metrics['node_periodic_iperf3_rx_bitrate{hostname="' .. hostname .. '"}'] = rx
-                metrics['node_periodic_iperf3_tx_bitrate{hostname="' .. hostname .. '"}'] = tx
+                metrics['node_periodic_iperf3_rx_bitrate{hostname="' .. hostname .. '",protocol="' .. protocol .. '"}'] = rx
+                metrics['node_periodic_iperf3_tx_bitrate{hostname="' .. hostname .. '",protocol="' .. protocol .. '"}'] = tx
             end
-		end
-	end
+        end
+    end
 
 end
 
--- utils/prometheus-exporter/files/service/periodic-metrics.lua
+++ utils/prometheus-exporter/files/service/periodic-metrics.lua
@@ -1,40 +1,38 @@
 #!/usr/bin/lua
 --[[
 
-	Part of AREDN -- Used for creating Amateur Radio Emergency Data Networks
-	Copyright (C) 2023 Tim Wilkinson
-	See Contributors file for additional contributors
+    Part of AREDN -- Used for creating Amateur Radio Emergency Data Networks
+    Copyright (C) 2023 Tim Wilkinson
+    See Contributors file for additional contributors
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation version 3 of the License.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation version 3 of the License.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-	Additional Terms:
+    Additional Terms:
 
-	Additional use restrictions exist on the AREDN(TM) trademark and logo.
-		See AREDNLicense.txt for more info.
+    Additional use restrictions exist on the AREDN(TM) trademark and logo.
+        See AREDNLicense.txt for more info.
 
-	Attributions to the AREDN Project must be retained in the source code.
-	If importing this code into a new or existing project attribution
-	to the AREDN project must be added to the source code.
+    Attributions to the AREDN Project must be retained in the source code.
+    If importing this code into a new or existing project attribution
+    to the AREDN project must be added to the source code.
 
-	You must not misrepresent the origin of the material contained within.
+    You must not misrepresent the origin of the material contained within.
 
-	Modified versions must be modified to attribute to the original source
-	and be marked in reasonable ways as differentiate it from the original
-	version
+    Modified versions must be modified to attribute to the original source
+    and be marked in reasonable ways as differentiate it from the original
+    version
 
 --]]
-
-local info = require("aredn.info")
 
 local metrics_conf = "/etc/metrics.conf"
 local metrics_data = "/tmp/periodic-metrics"
@@ -42,62 +40,78 @@ local metrics_periodics = "/usr/local/bin/metrics/periodics"
 local M = {}
 
 function M.periodic_metrics()
-	-- Only run if we have a configuration
-	if not nixio.fs.stat(metrics_conf) then
-		exit_app()
-		return
-	end
+    -- Only run if we have a configuration
+    if not nixio.fs.stat(metrics_conf) then
+        exit_app()
+        return
+    end
 
-	local periodics = {}
-	for name in nixio.fs.dir(metrics_periodics)
-	do
-		local task = name:match("^(.+)%.lua$")
-		if task then
-			periodics[#periodics + 1] = require("metrics.periodics." .. task)
-		end
-	end
+    wait_for_ticks(30) -- wait to let other things startup
 
-	local metrics = {}
-
-	-- Read the configuration
-	local conf = {}
-	for line in io.lines(metrics_conf)
-	do
-		local freq, name, arg = line:match("^%s*(%a*)%s+(%S+)%s+(%S+)%s*$")
-		if freq then
-			conf[#conf + 1] = { frequency = freq, name = name, arg = arg }
-		end
-	end
-
-	-- Run once per hour, but signal each 24 hour period
-	local count = 0
-	while true
+    local periodics = {}
+    for name in nixio.fs.dir(metrics_periodics)
     do
-		-- Call each periodic in turn
-		local options = { is_daily = (count == 0) }
-		for _, periodic in ipairs(periodics)
-		do
-			periodic(conf, metrics, options)
-		end
+        local task = name:match("^(.+)%.lua$")
+        if task then
+            periodics[#periodics + 1] = require("metrics.periodics." .. task)
+        end
+    end
 
-		-- Update metrics data for async retrieval
-		f = io.open(metrics_data, "w")
-		if f then
-			local keys = {}
-			for k, _ in pairs(metrics)
-			do
-				keys[#keys + 1] = k
-			end
-			table.sort(keys)
-			for _, k in ipairs(keys)
-			do
-				f:write(k .. " " .. metrics[k])
-			end
-			f:close()
-		end
+    local metrics = {}
 
-		count = (count + 1) % 24
-        wait_for_ticks(60 * 60) -- 1 hour
+    -- Read the configuration
+    local conf = {}
+    for line in io.lines(metrics_conf)
+    do
+        local freq, name, arg = line:match("^%s*(%a*)%s+(%S+)%s*(.+)%s*$")
+        if freq then
+            conf[#conf + 1] = { frequency = freq, name = name, arg = arg }
+        end
+    end
+
+    -- Run once per hour, but signal each 24 hour period
+    local count = 0
+    while true
+    do
+        local start = os.time()
+
+        -- Call each periodic in turn
+        local options = { is_daily = (count == 0) }
+        for _, periodic in ipairs(periodics)
+        do
+            periodic(conf, metrics, options)
+        end
+
+        -- Update metrics data for async retrieval
+        f = io.open(metrics_data, "w")
+        if f then
+            local keys = {}
+            for k, _ in pairs(metrics)
+            do
+                keys[#keys + 1] = k
+            end
+            table.sort(keys)
+            local lastprefix = nil
+            for _, k in ipairs(keys)
+            do
+                local prefix = k:match("^([^{%s]+)")
+                if prefix ~= lastprefix then
+                    lastprefix = prefix
+                    f:write("# HELP " .. prefix .. "\n# TYPE " .. prefix)
+                    if prefix:match("_total$") then
+                        f:write(" count\n")
+                    else
+                        f:write(" guage\n")
+                    end
+                end
+                f:write(k .. " " .. metrics[k] .. "\n")
+            end
+            f:close()
+        end
+
+        count = (count + 1) % 24
+
+        wait_for_ticks(math.max(60, 60 * 60 - (os.time() - start)))
     end
 end
 
