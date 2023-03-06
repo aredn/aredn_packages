7d60272da46d3820abb80fdaa18fe53e7e0d4e28
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2023-03-05 21:54:32 -0800

    Generalize periodics

--- utils/prometheus-exporter/files/service/periodic-metrics.lua
+++ utils/prometheus-exporter/files/service/periodic-metrics.lua
@@ -38,123 +38,64 @@ local info = require("aredn.info")
 
 local metrics_conf = "/etc/metrics.conf"
 local metrics_data = "/tmp/periodic-metrics"
-local myhostname = info.get_nvram("node") or "localnode"
+local metrics_periodics = "/usr/local/bin/metrics/periodics"
 local M = {}
-local metrics = {}
-
-function M.run_iperf3_metric(from, to, protocol)
-	local bitrate = nil
-	local retry = 3
-	while not bitrate and retry > 0
-	do
-		local f = io.popen("wget -q -O - 'http://" .. from .. ":8080/cgi-bin/iperf?server=" .. to .. "&protocol=" .. protocol .. "'")
-		if f then
-			for line in f:lines()
-			do
-				if line:match("iperf is disabled") then
-					break
-				end
-				if line:match("<title>BUSY</title>") then
-					f:close()
-					f = nil
-					retry = retry - 1
-					wait_for_ticks(math.floor(20 + 20 * math.random()))
-					break
-				end
-				local bitrate, bu = line:match("[%d%.]+-[%d%.]+%s+sec%s+[%d%.]+ [KM]Bytes%s+([%d%.]+) ([MK])bits/sec%s+receiver")
-				if bitrate then
-					bitrate = tonumber(bitrate) / (bu == "K" and 1024 or 1)
-					break
-				end
-				local bitrate, bu = line:match("[%d%.]+-[%d%.]+%s+sec%s+[%d%.]+ [KM]Bytes%s+([%d%.]+) ([MK])bits/sec%s+[%d%.]+ ms%s+%d+/%d+ %(([%d%.]+)%%%)%s+receiver")
-				if bitrate then
-					bitrate = tonumber(bitrate) / (bu == "K" and 1024 or 1)
-					break
-				end
-			end
-			if f then
-				f:close()
-			end
-		end
-	end
-	return bitrate
-end
-
-function M.run_metrics(is_daily)
-
-	-- Build a table of my peers on demand
-	local peers = nil
-	function is_peers(hostname)
-		if not peers then
-			local f = io.open("/tmp/lqm.info")
-			if not f then
-				return false
-			end
-			local lqm = luci.jsonc.parse(f:read("*a"))
-			f:close()
-			peers = {}
-			for _, tracker in pairs(lqm.trackers)
-			do
-				if tracker.hostname then
-					peers[tracker.hostname] = true
-				end
-			end
-		end
-		return peers[hostname]
-	end
-
-	for line in io.lines(metrics_conf)
-	do
-		local freq, type, args = line:match("^%s*(%a*)%s+(%S+)%s+(%S+)%s*$")
-		if freq == "hourly" or (freq == "daily" and is_daily) then
-			if type == "iperf3" then
-				local hostname, protocol = args:match("(%S+)%s+(%S+)")
-				if hostname and is_peer(hostname) then
-					if protocol ~= "udp" then
-						protocol = "tcp"
-					end
-					local rx = M.run_iperf3_metric(myhostname, hostname, protocol)
-					local tx = nil
-					if myhostname ~= "localnode" then
-						wait_for_ticks(1)
-						tx = M.run_iperf3_metric(hostname, myhostname, protocol)
-					end
-					metrics['node_periodic_iperf3_rx_bitrate{hostname="' .. hostname .. '"}'] = rx
-					metrics['node_periodic_iperf3_tx_bitrate{hostname="' .. hostname .. '"}'] = tx
-				end
-			end
-		end
-	end
-
-	-- Update metrics data for async retrieval
-	f = io.open(metrics_data, "w")
-	if f then
-		local keys = {}
-		for k, _ in pairs(metrics)
-		do
-			keys[#keys + 1] = k
-		end
-		table.sort(keys)
-		for _, k in ipairs(keys)
-		do
-			f:write(k .. " " .. metrics[k])
-		end
-		f:close()
-	end
-end
 
 function M.periodic_metrics()
-	-- Only run if we havw a configuration
+	-- Only run if we have a configuration
 	if not nixio.fs.stat(metrics_conf) then
 		exit_app()
 		return
+	end
+
+	local periodics = {}
+	for name in nixio.fs.dir(metrics_periodics)
+	do
+		local task = name:match("^(.+)%.lua$")
+		if task then
+			periodics[#periodics + 1] = require("metrics.periodics." .. task)
+		end
+	end
+
+	local metrics = {}
+
+	-- Read the configuration
+	local conf = {}
+	for line in io.lines(metrics_conf)
+	do
+		local freq, name, arg = line:match("^%s*(%a*)%s+(%S+)%s+(%S+)%s*$")
+		if freq then
+			conf[#conf + 1] = { frequency = freq, name = name, arg = arg }
+		end
 	end
 
 	-- Run once per hour, but signal each 24 hour period
 	local count = 0
 	while true
     do
-		M.run_metrics(count == 0)
+		-- Call each periodic in turn
+		local options = { is_daily = (count == 0) }
+		for _, periodic in ipairs(periodics)
+		do
+			periodic(conf, metrics, options)
+		end
+
+		-- Update metrics data for async retrieval
+		f = io.open(metrics_data, "w")
+		if f then
+			local keys = {}
+			for k, _ in pairs(metrics)
+			do
+				keys[#keys + 1] = k
+			end
+			table.sort(keys)
+			for _, k in ipairs(keys)
+			do
+				f:write(k .. " " .. metrics[k])
+			end
+			f:close()
+		end
+
 		count = (count + 1) % 24
         wait_for_ticks(60 * 60) -- 1 hour
     end
